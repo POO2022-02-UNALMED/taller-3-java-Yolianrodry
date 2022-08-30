708e6f0732513509b2941355e2807e8e19bcf241
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-08-29 23:08:11 -0500

    TV

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,16 +1,14 @@
 package taller3.televisores;
 
 public class Marca{
-
-	 String nombre;
-
-		public Marca(String nombre) {
-			this.nombre = nombre;
-		}
-		public String getNombre() {
-			return nombre;
-		}
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}	
-	}
+    String nombre;
+    public Marca(String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+}

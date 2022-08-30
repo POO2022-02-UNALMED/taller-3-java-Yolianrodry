3f1f4a6b1fabdf882ac7d932047073b2f6684ed0
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-08-29 22:16:38 -0500

    T1

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -2,4 +2,50 @@ package taller3.televisores;
 
 public class TV {
 
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	boolean estado;
+	private int volumen = 1;
+	private Control control;
+	private static int numTV;
+		
+	public static int getNumTV() {
+		return numTV;
+	}
+
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		numTV ++;
+	}
+	
+	public static void setNumTV(int numTV) {
+		TV.numTV = numTV;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
 }

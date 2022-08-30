6d818c1393ac49c15cd983616b076ac9ed11addf
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-08-29 21:54:18 -0500

    M2

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -7,14 +7,13 @@ public class Marca {
 	public Marca(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 	public String getNombre() {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	
 }
